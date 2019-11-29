8b96eb671bc188e7ed44174c12178ec18c91578d
elmernocon <elmernocon@gmail.com>
2019-11-29 18:52:50 +0800

    Added C# files in the test Unity project.

---
+++ tests/unity/Assets/Scripts/MyClass.cs
@@ -0,0 +1,8 @@
+﻿public class MyClass
+{
+    #region Fields
+
+    public string Greeting = "Hello World";
+
+    #endregion
+}
---
+++ tests/unity/Assets/Scripts/MyMonoBehaviour.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class MyMonoBehaviour : MonoBehaviour
+{
+    #region Fields
+
+    [SerializeField] private string _greeting = "Hello World";
+
+    #endregion
+
+    #region Methods
+
+    private void Start()
+    {
+        Debug.Log(_greeting);
+    }
+
+    #endregion
+}
